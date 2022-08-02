a2bf7086c429d60eca65a0a07fe7acb648915d42
Jacek Korzemski <wrp40.000@wp.pl>
2022-08-02 23:36:17 +0200

    updated display for mobile phones

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-	// const host = "https://dice-roller-node.herokuapp.com";
-	const host = "http://localhost:5000";
+	const host = "https://dice-roller-node.herokuapp.com";
+	// const host = "http://localhost:5000";
 	let status = "Gotowy do rzutu";
 	let uiActive = true;
 	let name = "Gal Anonim";
 	let customRoll = "";
 	let rolls = [];
+	let opened = "";
 
 	const sendRoll = (user, dices) => {
 		uiActive = false;
@@ -53,61 +54,131 @@
 </script>
 
 <main>
-	<h2>Gracz:</h2>
-	<input type="text" bind:value={name} />
-	<hr />
-	<h2>Status: <br />{status}</h2>
-	<hr />
-	<h2>Kostki:</h2>
-	<div class={uiActive ? "ready" : "not-ready"}>
-		<div class="grid-row">
-			<button on:click={() => sendRoll(name, "d4")}>d4</button>
-			<button on:click={() => sendRoll(name, "d6")}>d6</button>
-			<button on:click={() => sendRoll(name, "d8")}>d8</button>
-			<button on:click={() => sendRoll(name, "d10")}>d10</button>
-			<button on:click={() => sendRoll(name, "d12")}>d12</button>
-			<button on:click={() => sendRoll(name, "d20")} class="main">d20</button>
-			<button on:click={() => sendRoll(name, "d100")}>d100</button>
+	<div class="main">
+		<div class="top-part-of-the-app">
+			<h3>Gracz:</h3>
+			<input type="text" bind:value={name} />
+			<hr />
+			<h3>Status: {status}</h3>
+			<hr />
+			<h4>Kostki:</h4>
+			<div class={uiActive ? "ready" : "not-ready"}>
+				<div style="display: flex; justify-content: space-between; gap: 15px;">
+					<p
+						class="clickable"
+						on:click={() => {
+							opened === "basic" ? (opened = "") : (opened = "basic");
+						}}
+					>
+						Podstawowe {#if opened === "basic"} 🔽 {:else} 🔼 {/if}
+					</p>
+
+					<p
+						class="clickable"
+						on:click={() => {
+							opened === "popular" ? (opened = "") : (opened = "popular");
+						}}
+					>
+						Często używane {#if opened === "popular"} 🔽 {:else} 🔼 {/if}
+					</p>
+
+					<p
+						class="clickable"
+						on:click={() => {
+							opened === "custom" ? (opened = "") : (opened = "custom");
+						}}
+					>
+						Własna formuła {#if opened === "custom"} 🔽 {:else} 🔼 {/if}
+					</p>
+				</div>
+
+				{#if opened === "basic"}
+					<div class="grid-row">
+						<button on:click={() => sendRoll(name, "d4")}>d4</button>
+						<button on:click={() => sendRoll(name, "d6")}>d6</button>
+						<button on:click={() => sendRoll(name, "d8")}>d8</button>
+						<button on:click={() => sendRoll(name, "d10")}>d10</button>
+						<button on:click={() => sendRoll(name, "d12")}>d12</button>
+						<button on:click={() => sendRoll(name, "d20")} class="main">d20</button>
+						<button on:click={() => sendRoll(name, "d100")}>d100</button>
+					</div>
+				{/if}
+
+				{#if opened === "popular"}
+					<div class="grid-row">
+						<button on:click={() => sendRoll(name, "2d6")}>2d6</button>
+					</div>
+				{/if}
+
+				{#if opened === "custom"}
+					<p>
+						<small>Np.: 2d6 + d4 + 1</small>
+					</p>
+					<div class="input-grid">
+						<input type="text" bind:value={customRoll} />
+						<button on:click={() => sendRoll(name, customRoll)}>Rzuć!</button>
+					</div>
+				{/if}
+			</div>
+		</div>
+		<div class="bottom-part-of-the-app">
+			<h3>Rzuty:</h3>
+
+			<ul>
+				{#each rolls as roll}
+					<li>
+						<div class="roll">
+							<div class="top">
+								<span>rzut numer <strong class="green">{roll.id}</strong>: </span>
+								<span>gracz: <strong>{roll.user}</strong></span>
+							</div>
+							<div class="middle">
+								<span>wyrzucił: <strong>{roll.result}</strong></span>
+								<span>używając: <strong>{roll.dices}</strong></span>
+							</div>
+							<div class="bottom">
+								<span>
+									Szczegółowo:
+									{#each roll.details as detail}
+										{detail},
+									{/each}
+								</span>
+							</div>
+						</div>
+					</li>
+				{/each}
+			</ul>
 		</div>
 	</div>
-	<h2>Własna formuła</h2>
-	<p>
-		<small>Np.: 2d6 + d4 + 1</small>
-	</p>
-	<div class="input-grid">
-		<input type="text" bind:value={customRoll} />
-		<button on:click={() => sendRoll(name, customRoll)}>Rzuć!</button>
-	</div>
-
-	<h3>Rzuty:</h3>
-
-	<ul>
-		{#each rolls as roll}
-			<li>
-				<div class="roll">
-					<div class="top">
-						<span>rzut numer <strong class="green">{roll.id}</strong>: </span>
-						<span>gracz: <strong>{roll.user}</strong></span>
-					</div>
-					<div class="middle">
-						<span>wyrzucił: <strong>{roll.result}</strong></span>
-						<span>używając: <strong>{roll.dices}</strong></span>
-					</div>
-					<div class="bottom">
-						<span>
-							Szczegółowo:
-							{#each roll.details as detail}
-								{detail},
-							{/each}
-						</span>
-					</div>
-				</div>
-			</li>
-		{/each}
-	</ul>
 </main>
 
 <style>
+	.top-part-of-the-app {
+		height: 100%;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+		padding-bottom: 15px;
+	}
+	.bottom-part-of-the-app {
+		padding: 15px;
+		width: calc(100% - 30px);
+		height: 100%;
+		overflow-y: auto;
+	}
+
+	.clickable {
+		cursor: pointer;
+	}
+	.main {
+		max-width: calc(100% - 15px);
+		margin: 0 auto;
+		width: 640px;
+		max-height: 100vh;
+		gap: 15px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+	}
 	.green {
 		color: green;
 	}
@@ -149,6 +220,7 @@
 		top: 0;
 		left: 0;
 		width: 100%;
+		max-width: 100%;
 		min-height: 100%;
 	}
 	ul {
@@ -173,7 +245,7 @@
 		max-width: 480px;
 		display: grid;
 		margin: 10px auto;
-		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
 		gap: 10px;
 		text-transform: uppercase;
 	}
@@ -183,6 +255,7 @@
 		width: 100%;
 	}
 	button.main {
+		width: 100%;
 		background: rgba(16, 255, 15, 0.8);
 	}
 	.input-grid {
